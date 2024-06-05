5c443be0bcc22b642881327b9e4cb5b781f75866
ANKITA <anki2gmail.com>
2024-06-05 15:58:52 +0530

    AddRemoveItemsInCart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,21 +1,39 @@
-import React, { useContext } from "react";
+import { useContext, useEffect, useState } from 'react';
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.quantity;
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
     const handleOpenCart = () => {
         props.setIsOpen(true);
     };
 
     return (
-        <button className={classes.button} onClick={handleOpenCart}>
+        <button className={btnClasses} onClick={handleOpenCart}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
--- src/components/Meals/MealForm.js
+++ src/components/Meals/MealForm.js
@@ -1,24 +1,32 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import classes from './MealForm.module.css';
 import Input from '../UI/Input';
 import CartContext from '../../store/cart-context';
 
 export default function MealForm(props) {
     const cartCtx = useContext(CartContext);
-    const [amount, setAmount] = useState(0);
+    const cartItem = cartCtx.items.find(item => item.name === props.item.name);
+    const initialAmount = cartItem ? cartItem.quantity : 0;
+    const [amount, setAmount] = useState(initialAmount);
+
+    useEffect(() => {
+        if (cartItem) {
+            setAmount(cartItem.quantity);
+        }
+    }, [cartItem]);
 
     const addToCart = (e) => {
         e.preventDefault();     
     };
     const incrementAmount = () => {
         setAmount((prevAmount) => prevAmount + 1);
-        cartCtx.addItem({ ...props.item, quantity: amount });
+        cartCtx.addItem({ ...props.item, quantity: 1 });
     };
     const decrementAmount = () => {
         if (amount > 0) {
             const newAmount = amount - 1;
             setAmount(newAmount);
-            cartCtx.removeItem({ ...props.item, quantity: newAmount });
+            cartCtx.removeItem({ ...props.item, quantity: 1 });
         }
     };
     return (
@@ -36,7 +44,7 @@ export default function MealForm(props) {
                 }}
             />
             <button type="button" className={classes.left} onClick={incrementAmount}>+</button>
-            <span>ADD</span>
+            <span className={classes.addtext}>ADD</span>
             <button type="button" className={classes.right} onClick={decrementAmount}>-</button>
         </form>
     );
